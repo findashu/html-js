32362bcb5caddc629f16a070fc36becd354ddcfa
Ashu_Mishra <ashutosh@thruskills.com>
2019-05-03 17:17:31 +0530

    started css

---
+++ js/index.js
@@ -0,0 +1,24 @@
+var a = 10;
+var A = 20;
+
+// Case Sensetive
+var a12 = 10;
+var _hello = 40;
+
+var _ = 30;
+
+// Representing a constant value
+
+var PI = 3.14;
+var user_Name = 'Ashutosh';
+var userLastName = '';
+
+console.log(_)
+
+console.log(_hello)
+
+console.log(a);
+console.log(A);
+console.log(a12);
+
+console.log('External Javascript File')
